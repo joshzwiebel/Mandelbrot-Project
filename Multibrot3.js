f0e598f2ab8103f1dfb8452a5b0919ac5f0a7138
joshzwiebel <jjzwiebe@edu.uwaterloo.ca>
2019-03-10 10:19:00 -0400

    added a file to work on a third degree multibrot

---
+++ Multibrot3.js
@@ -0,0 +1,8 @@
+function setup(){
+    createCanvas(720,720);
+    pixelDensity(1)
+}
+
+function draw(){
+    //todo create a 3rd degree mandelbrot that appears on button press. potentially general formula but we will see
+}
--- Starter_Javascript.js
+++ Starter_Javascript.js
@@ -52,6 +52,7 @@ function draw() {
     }
 
 
+
     updatePixels();
 
 }
