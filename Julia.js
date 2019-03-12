554c81ae6734ba9ed34ac869fd54b1ca862a11cf
lujack <jacklu0831@gmail.com>
2019-03-11 23:46:12 -0400

    Julia set (non-multi)

--- Julia.js
+++ Julia.js
@@ -1,42 +1,41 @@
-var minSlider;
-var maxSlider;
+let angle = 0;
 
 function setup() {
-    createCanvas(360, 360);
+    createCanvas(360,360);
     pixelDensity(1);
-    minSlider = createSlider(-2.5, 0, -2.5, 0.01);
-    maxSlider = createSlider(0, 2.5, 2.5, 0.01);
 }
 
 function draw() {
-    loadPixels();
 
-    for (var x = 0; x < width; x++) {
-        for (var y = 0; y < height; y++) {
-            var a = map(x, 0, width, minSlider.value(), maxSlider.value());
-            var b = map(y, 0, height, minSlider.value(), maxSlider.value());
+    loadPixels();
+    let cx = map(mouseX, 0, width, -1, 1);
+    let cy = map(mouseY, 0, height, -1, 1);
+
+    angle += 0.00001;
+
+    for (let x = 0; x < width; x++) {
+        for (let y = 0; y < height; y++) {
+            let a = map(x, 0, width, -2.5, 2.5);
+            let b = map(y, 0, height, -2.5, 2.5);
             /*var a = map(x, 0, width, -2.5, 2.5);
             var b = map(y, 0, height, -2.5, 2.5);*/
             var n = 0;
-            var ca = a;
-            var cb = b;
 
             while (n < 50) {
-                var aa = a * a - b * b;
-                var bb = 2 * a * b;
-                a = aa + ca;
-                b = bb + cb;
-                if (abs(a + b) > 16) {
+                if (a * a + b * b > 4.0) {   // abs() is not needed since both positive
                     break;
                 }
+                let twoAb = 2 * a * b;
+                a = a * a - b * b + cx;
+                b = twoAb + cy;
                 n++;
+                angle += 0.1;
             }
             var bright = map(n, 0, 50, 0, 1);
             bright = map(sqrt(bright), 0, 1, 0, 255);
             if (n === 50) {
                 bright = 0;
             }
-
 
             var pix = (x + y * width) * 4;
             pixels[pix + 0] = bright;
@@ -46,8 +45,5 @@ function draw() {
 
         }
     }
-
     updatePixels();
-    noloop();
-
 }
--- Multibrot.js
+++ Multibrot.js
@@ -1,8 +1,5 @@
-
 var minSlider;
 var maxSlider;
-
-
 
 
 function setup() {
@@ -10,29 +7,18 @@ function setup() {
     pixelDensity(1);
     minSlider = createSlider(-2.5, 0, -2.5, 0.5);
     maxSlider = createSlider(0, 2.5, 2.5, 0.5);
-
-
-
 }
 
 
 function display() {
     loadPixels();
 
-
-
     for (var x = 0; x < width; x++) {
         for (var y = 0; y < height; y++) {
             let current_real = map(x, 0, width, minSlider.value(), maxSlider.value());
             let current_imag = map(y, 0, height, minSlider.value(), maxSlider.value());
 
-
-
-
-
-            var  num =  new Complex(current_real, current_imag);
-
-
+            var num = new Complex(current_real, current_imag);
 
             /*var a = map(x, 0, width, -2.5, 2.5);
             var b = map(y, 0, height, -2.5, 2.5);*/
@@ -42,14 +28,9 @@ function display() {
             var expbutton = document.getElementById("userInput");
             var exponent = expbutton.value;
 
-
-
-
-
             while (n < 10) {
 
                 num.exp(exponent);
-
 
                 num.display_complex();
 
@@ -57,33 +38,27 @@ function display() {
 
                 num.imaginary += starting_imag;
 
-
                 if (num.abs() > 16) {
                     break;
                 }
+
                 n++;
-
-
             }
+
             var bright = map(n, 0, 50, 0, 1);
             bright = map(Math.sqrt(bright), 0, 1, 0, 255);
             if (n === 10) {
                 bright = 0;
             }
 
-
             var pix = (x + y * width) * 4;
             pixels[pix] = bright;
             pixels[pix + 1] = bright;
             pixels[pix + 2] = bright;
             pixels[pix + 3] = 255;
-
-
         }
     }
 
-
     updatePixels();
     //noLoop();
-
 }
