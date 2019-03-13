19b9adf8c73b322c5826643813102540fb58a6b0
joshzwiebel <jjzwiebe@edu.uwaterloo.ca>
2019-03-13 00:12:46 -0400

    added hsv/rgb converter. Added color mapping scheme
    The mandelbrot set is not colored using a smooth coloring slgoirhtm, colors are in a gradient with a nice smooth curve. It is similar to escape time algorithm except it is smooth

--- Multibrot.js
+++ Multibrot.js
@@ -23,55 +23,50 @@ function display() {
 
 
             var num = new Complex(current_real, current_imag);
-
-
             /*var a = map(x, 0, width, -2.5, 2.5);
             var b = map(y, 0, height, -2.5, 2.5);*/
-            let n = 0;
-            let starting_imag = current_imag;
-            let starting_real = current_real;
+            let iterations = 0;
+
+            let start = new Complex(current_real,current_imag);
             var expbutton = document.getElementById("userInput");
             var exponent = expbutton.value;
+            let max_iterations = 100;
+            let final_num = new Complex(0,0);
 
-
-            while (n < 10) {
-
+            while (iterations < max_iterations) {
                 num.exp(exponent);
 
-
-                num.display_complex();
-
-                num.real += starting_real;
-
-                num.imaginary += starting_imag;
-
-
-                if (num.abs() > 16) {
+                num.add(start);
+                if (num.abs() > 4) { //todo check what this boundary should be
+                    final_num = num;
                     break;
+
                 }
-                n++;
-
+                iterations++;
+                final_num = num;
 
             }
-            var bright = map(n, 0, 50, 0, 1);
-            bright = map(Math.sqrt(bright), 0, 1, 0, 255);
-            if (n === 10) {
-                bright = 0;
+            let logzn = Math.log(final_num.abs())/exponent; //this will cause issues for exponents of 1/0/negative
+            let nu = Math.log(logzn/Math.log(exponent))/Math.log(exponent);
+            let rgb = [0,0,0];
+            let bright = iterations+1-nu;
+            //let bright = map(iterations, 0, max_iterations, 0, 1);
+            /*bright = map(Math.sqrt(bright), 0, 1, 0, 255);*/
+            //bright = map(bright, 0, 1, 0, 255);
+            if (iterations === max_iterations) {
+                rgb=[0,0,0];
             }
-
+            else{
+                rgb = hsvtorgb(bright,1,1)
+            }
 
             var pix = (x + y * width) * 4;
-            pixels[pix] = bright;
-            pixels[pix + 1] = bright;
-            pixels[pix + 2] = bright;
-            pixels[pix + 3] = 255;
-
-
+            pixels[pix] = rgb[0];
+            pixels[pix + 1] = rgb[1];
+            pixels[pix + 2] = rgb[2];
+            pixels[pix + 3] = iterations*255*100/max_iterations; //this line can be changed to simply 255 for ful brightness
         }
     }
-
-
     updatePixels();
     //noLoop();
-
 }
