24ff68f4ea0f84dd0a2ef6fb81640c33b688b6fb
joshzwiebel <jjzwiebe@edu.uwaterloo.ca>
2019-04-30 20:15:35 -0400

    multiple updates relating to organization and formatting
    renamed files to make more sense. added code to the separate html pages. deleted superfluous pages. added extra files to a separate libraries folder. added reference to home page

--- LibrariesandGlobals/p5.dom.js
+++ LibrariesandGlobals/p5.dom.js
@@ -27,10 +27,10 @@
 
 (function(root, factory) {
   if (typeof define === 'function' && define.amd)
-    define('p5.dom', ['p5'], function(p5) {
+    define('p5.dom', ['LibrariesandGlobals/p5'], function(p5) {
       factory(p5);
     });
-  else if (typeof exports === 'object') factory(require('../p5'));
+  else if (typeof exports === 'object') factory(require('./p5'));
   else factory(root['p5']);
 })(this, function(p5) {
   // =============================================================================
