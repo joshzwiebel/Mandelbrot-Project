74b635d0354395650d32f6c4654cd9ed444b3881
joshzwiebel <jjzwiebe@edu.uwaterloo.ca>
2019-05-01 21:04:32 -0400

    changed the setup of mandelbrot page to lookbetter adn made github pages work

--- Multibrot.js
+++ Multibrot.js
@@ -3,7 +3,9 @@ var maxSlider;
 
 
 function setup() {
-    createCanvas(360, 360);
+    var cnv = createCanvas(720, 720);
+    cnv.position((displayWidth-720)/2,300);
+
     pixelDensity(1);
     minSlider = createSlider(-2.5, 0, -2.5, 0.5);
     maxSlider = createSlider(0, 2.5, 2.5, 0.5);
@@ -11,6 +13,7 @@ function setup() {
 
 
 }
+
 function initdisplay() {
     loadPixels();
 
@@ -26,11 +29,11 @@ function initdisplay() {
             var b = map(y, 0, height, -2.5, 2.5);*/
             let iterations = 0;
 
-            let start = new Complex(current_real,current_imag);
+            let start = new Complex(current_real, current_imag);
 
             var exponent = 2
             let max_iterations = 100;
-            let final_num = new Complex(0,0);
+            let final_num = new Complex(0, 0);
 
             while (iterations < max_iterations) {
                 num.exp(exponent);
@@ -45,25 +48,24 @@ function initdisplay() {
                 final_num = num;
 
             }
-            let logzn = Math.log(final_num.abs())/exponent; //this will cause issues for exponents of 1/0/negative
-            let nu = Math.log(logzn/Math.log(exponent))/Math.log(exponent);
-            let rgb = [0,0,0];
-            let bright = iterations+1-nu;
+            let logzn = Math.log(final_num.abs()) / exponent; //this will cause issues for exponents of 1/0/negative
+            let nu = Math.log(logzn / Math.log(exponent)) / Math.log(exponent);
+            let rgb = [0, 0, 0];
+            let bright = iterations + 1 - nu;
             //let bright = map(iterations, 0, max_iterations, 0, 1);
             /*bright = map(Math.sqrt(bright), 0, 1, 0, 255);*/
             //bright = map(bright, 0, 1, 0, 255);
             if (iterations === max_iterations) {
-                rgb=[0,0,0];
-            }
-            else{
-                rgb = hsvtorgb(bright,1,1)
+                rgb = [0, 0, 0];
+            } else {
+                rgb = hsvtorgb(bright, 1, 1)
             }
 
             var pix = (x + y * width) * 4;
             pixels[pix] = rgb[0];
             pixels[pix + 1] = rgb[1];
             pixels[pix + 2] = rgb[2];
-            pixels[pix + 3] = iterations*255*100/max_iterations; //this line can be changed to simply 255 for ful brightness
+            pixels[pix + 3] = iterations * 255 * 100 / max_iterations; //this line can be changed to simply 255 for ful brightness
         }
     }
     updatePixels();
@@ -85,11 +87,11 @@ function display() {
             var b = map(y, 0, height, -2.5, 2.5);*/
             let iterations = 0;
 
-            let start = new Complex(current_real,current_imag);
+            let start = new Complex(current_real, current_imag);
             var expbutton = document.getElementById("userInput");
             var exponent = expbutton.value;
             let max_iterations = 100;
-            let final_num = new Complex(0,0);
+            let final_num = new Complex(0, 0);
 
             while (iterations < max_iterations) {
                 num.exp(exponent);
@@ -104,25 +106,24 @@ function display() {
                 final_num = num;
 
             }
-            let logzn = Math.log(final_num.abs())/exponent; //this will cause issues for exponents of 1/0/negative
-            let nu = Math.log(logzn/Math.log(exponent))/Math.log(exponent);
-            let rgb = [0,0,0];
-            let bright = iterations+1-nu;
+            let logzn = Math.log(final_num.abs()) / exponent; //this will cause issues for exponents of 1/0/negative
+            let nu = Math.log(logzn / Math.log(exponent)) / Math.log(exponent);
+            let rgb = [0, 0, 0];
+            let bright = iterations + 1 - nu;
             //let bright = map(iterations, 0, max_iterations, 0, 1);
             /*bright = map(Math.sqrt(bright), 0, 1, 0, 255);*/
             //bright = map(bright, 0, 1, 0, 255);
             if (iterations === max_iterations) {
-                rgb=[0,0,0];
-            }
-            else{
-                rgb = hsvtorgb(bright,1,1)
+                rgb = [0, 0, 0];
+            } else {
+                rgb = hsvtorgb(bright, 1, 1)
             }
 
             var pix = (x + y * width) * 4;
             pixels[pix] = rgb[0];
             pixels[pix + 1] = rgb[1];
             pixels[pix + 2] = rgb[2];
-            pixels[pix + 3] = iterations*255*100/max_iterations; //this line can be changed to simply 255 for ful brightness
+            pixels[pix + 3] = iterations * 255 * 100 / max_iterations; //this line can be changed to simply 255 for ful brightness
         }
     }
     updatePixels();
